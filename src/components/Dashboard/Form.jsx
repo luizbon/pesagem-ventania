48bb1b79c543e70ccc43f4a25aa909a5dcdb79e3
Luiz Adilson da Silva Bon <luiz.bon@gmail.com>
2019-01-06 23:02:19 +1100

    Fix date

--- src/components/Dashboard/Form.jsx
+++ src/components/Dashboard/Form.jsx
@@ -4,98 +4,102 @@ import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import FormClass from "./models/FormClass";
 
 class InputForm extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = new FormClass();
+        this.state = new FormClass();
 
-    this.addAnimal = this.addAnimal.bind(this);
-    this.registroChange = this.registroChange.bind(this);
-    this.pesoChange = this.pesoChange.bind(this);
-    this.dataChange = this.dataChange.bind(this);
-  }
+        this.addAnimal = this.addAnimal.bind(this);
+        this.registroChange = this.registroChange.bind(this);
+        this.pesoChange = this.pesoChange.bind(this);
+        this.dataChange = this.dataChange.bind(this);
+    }
 
-  registroChange(e) {
-    this.setState({
-      registro: e.target.value.toUpperCase()
-    });
-  }
+    registroChange(e) {
+        this.setState({
+            registro: e.target.value.toUpperCase()
+        });
+    }
 
-  pesoChange(e) {
-    this.setState({
-      peso: e.target.value
-    });
-  }
+    pesoChange(e) {
+        this.setState({
+            peso: e.target.value
+        });
+    }
 
-  dataChange(e) {
-    this.setState({
-      data: e.target.value
-    });
-  }
+    dataChange(e) {
+        this.setState({
+            data: e.target.value
+        });
+    }
 
-  addAnimal(e) {
-    e.preventDefault();
-    const animal = new FormClass(this.state);
-    if (!animal.isValid) return;
+    addAnimal(e) {
+        e.preventDefault();
+        const animal = new FormClass(this.state);
+        if (!animal.isValid) return;
 
-    this.props.onSubmit(animal.toAnimal());
-    this.setState(new FormClass());
-    ReactDOM.findDOMNode(this.registro).focus();
-  }
+        this.props.onSubmit(animal.toAnimal());
+        this.setState(
+            new FormClass({
+                data: this.state.data
+            })
+        );
+        ReactDOM.findDOMNode(this.registro).focus();
+    }
 
-  render() {
-    return (
-      <Form className="m-2" onSubmit={this.addAnimal}>
-        <FormGroup>
-          <Label for="animal" className="mr-sm-2">
-            Animal
-          </Label>
-          <Input
-            type="text"
-            name="animal"
-            id="registro"
-            placeholder="Animal"
-            ref={input => {
-              this.registro = input;
-            }}
-            autoFocus
-            value={this.state.registro}
-            onChange={this.registroChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="peso" className="mr-sm-2">
-            Peso
-          </Label>
-          <Input
-            type="number"
-            name="peso"
-            id="peso"
-            placeholder="Peso"
-            value={this.state.peso}
-            onChange={this.pesoChange}
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="peso" className="mr-sm-2">
-            Data
-          </Label>
-          <Input
-            type="text"
-            name="data"
-            id="data"
-            value={this.state.data}
-            onChange={this.dataChange}
-            tabIndex="-1"
-            required
-          />
-        </FormGroup>
-        <Button type="submit">Gravar</Button>
-      </Form>
-    );
-  }
+    render() {
+        return (
+            <Form className="m-2" onSubmit={this.addAnimal}>
+                <FormGroup>
+                    <Label for="animal" className="mr-sm-2">
+                        Animal
+                    </Label>
+                    <Input
+                        type="text"
+                        name="animal"
+                        id="registro"
+                        placeholder="Animal"
+                        ref={input => {
+                            this.registro = input;
+                        }}
+                        autoFocus
+                        value={this.state.registro}
+                        onChange={this.registroChange}
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="peso" className="mr-sm-2">
+                        Peso
+                    </Label>
+                    <Input
+                        type="number"
+                        name="peso"
+                        id="peso"
+                        placeholder="Peso"
+                        value={this.state.peso}
+                        onChange={this.pesoChange}
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="peso" className="mr-sm-2">
+                        Data
+                    </Label>
+                    <Input
+                        type="text"
+                        name="data"
+                        id="data"
+                        value={this.state.data}
+                        onChange={this.dataChange}
+                        tabIndex="-1"
+                        required
+                    />
+                </FormGroup>
+                <Button type="submit">Gravar</Button>
+            </Form>
+        );
+    }
 }
 
 export default InputForm;
--- src/components/Dashboard/models/Animal.js
+++ src/components/Dashboard/models/Animal.js
@@ -3,43 +3,45 @@ import { formatDatabase } from "../../../shared/constants";
 import { guid } from "../../../shared/utils";
 
 export default class Animal {
-  constructor({ id, registro, peso, data }) {
-    this._id = id || guid();
-    this.registro = registro;
-    this.pesoFinal = peso;
-    this.dataAtual = data;
-    this.pesoInicial = null;
-    this.dataAnterior = null;
-  }
+    constructor({ id, registro, peso, data }) {
+        this._id = id || guid();
+        this.registro = registro;
+        this.pesoFinal = peso;
+        this.dataAtual = data;
+        this.pesoInicial = null;
+        this.dataAnterior = null;
+    }
 
-  static NewAnimal({
-    _id,
-    registro,
-    pesoFinal,
-    dataAtual,
-    pesoInicial,
-    dataAnterior
-  }) {
-    const animal = new Animal({});
-    animal._id = _id;
-    animal.registro = registro;
-    animal.pesoFinal = pesoFinal;
-    animal.dataAtual = dataAtual;
-    animal.pesoInicial = pesoInicial;
-    animal.dataAnterior = dataAnterior || null;
-    return animal;
-  }
+    static NewAnimal({
+        _id,
+        registro,
+        pesoFinal,
+        dataAtual,
+        pesoInicial,
+        dataAnterior
+    }) {
+        const animal = new Animal({});
+        animal._id = _id;
+        animal.registro = registro;
+        animal.pesoFinal = pesoFinal;
+        animal.dataAtual = dataAtual;
+        animal.pesoInicial = pesoInicial;
+        animal.dataAnterior = dataAnterior || null;
+        return animal;
+    }
 
-  get dias() {
-    if (this.dataAnterior === null) return null;
-    return moment(this.dataAtual, formatDatabase).diff(
-      moment(this.dataAnterior, formatDatabase),
-      "days"
-    );
-  }
+    get dias() {
+        if (this.dataAnterior === null) return null;
+        return moment(this.dataAtual, formatDatabase).diff(
+            moment(this.dataAnterior, formatDatabase),
+            "days"
+        );
+    }
 
-  get gdp() {
-    if (this.dataAnterior === null) return null;
-    return ((this.pesoFinal - this.pesoInicial) / this.dias) * 1000;
-  }
+    get gdp() {
+        if (this.dataAnterior === null) return null;
+        return parseInt(
+            ((this.pesoFinal - this.pesoInicial) / this.dias) * 1000
+        );
+    }
 }
