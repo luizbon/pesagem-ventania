c961eb04cc61f7e63c9294e3882188b620663981
Luiz Bon <292532+luizbon@users.noreply.github.com>
2023-01-02 21:48:27 +1100

    upgrade references

--- src/firebase/database.js
+++
@@ -1,6 +0,0 @@
-import Rebase from "re-base";
-import { database } from "./firebase";
-
-const base = Rebase.createClass(database);
-
-export { base };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 import config from "./config";
 
-!firebase.apps.length && firebase.initializeApp(config);
+const app = initializeApp(config);
 
-const auth = firebase.auth();
-const database = firebase.database();
+const auth = getAuth(app);
+const database = getDatabase(app);
 
 export { auth, database };
