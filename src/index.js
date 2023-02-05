e7953440f8b0f340b7d8eb2b6ec37ca6cc22e10f
Luiz Bon <292532+luizbon@users.noreply.github.com>
2023-02-06 10:53:49 +1100

    add sentry

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/print.css";
 import registerServiceWorker from "./registerServiceWorker";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+if (
+    process.env.NODE_ENV === "production" &&
+    process.env.REACT_APP_SENTRY_RELEASE
+) {
+    Sentry.init({
+        dsn: "https://b157cfbe30684d499e7caa33a61d9954@o1217558.ingest.sentry.io/4504625308434432",
+        integrations: [new BrowserTracing()],
+        tracesSampleRate: 1.0,
+        release: process.env.REACT_APP_SENTRY_RELEASE
+    });
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
